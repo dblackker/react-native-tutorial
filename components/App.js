fcc2d2bb5f8140bc8e794c6a5428aa5ca24e114a
Daniel Black <daniel.black@carsforsale.com>
2016-09-26 16:29:26 -0500

    Handle textinput

--- components/App.js
+++ components/App.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react'
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, TextInput, Text } from 'react-native';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
   render() {
     return (
-      <View style={{ flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-       }}>
-        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
-        <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
-        <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
+      <View style={{ padding: 10 }}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here to translate!"
+          onChangeText={text => this.setState({ text })}
+        />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {this.state.text.split(' ').map(word => word && '🍕').join(' ')}
+        </Text>
       </View>
     );
   }
