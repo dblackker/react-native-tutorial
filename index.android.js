28c84e9da39a293565c0574bd16d4614042aecbd
Daniel Black <daniel.black@carsforsale.com>
2016-09-26 12:39:26 -0500

    Flex direction

--- index.android.js
+++ index.android.js
@@ -4,7 +4,7 @@ import { AppRegistry, View } from 'react-native';
 class AwesomeProject extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, flexDirection: 'row'}}>
         <View style={{flex: 1, backgroundColor: 'powderblue'}} />
         <View style={{flex: 2, backgroundColor: 'skyblue'}} />
         <View style={{flex: 3, backgroundColor: 'steelblue'}} />
