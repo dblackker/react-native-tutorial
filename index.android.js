6b1564994219334474f819b597d95f2c058660ef
Daniel Black <daniel.black@carsforsale.com>
2016-09-26 12:27:45 -0500

    Using setting styles via stylesheets

--- index.android.js
+++ index.android.js
@@ -1,36 +1,28 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Text } from 'react-native';
-
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showText: true }
-
-    // Toggle state every second
-    setInterval(() => {
-      this.setState({ showText: !this.state.showText });
-    }, 500);
-  }
-
-  render() {
-    let display = this.state.showText ? this.props.text : ' ';
-    return (
-      <Text>{display}</Text>
-    );
-  }
-}
+import { AppRegistry, StyleSheet, View, Text } from 'react-native';
 
 class AwesomeProject extends Component {
   render() {
     return (
       <View>
-        <Blink text='I love to blink.' />
-        <Blink text='Yes blinking is so great.' />
-        <Blink text='Why did they ever take this out of HTML?' />
-        <Blink text='Look at me look at me' />
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
