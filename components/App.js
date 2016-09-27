b05b3d5e6ef017d9ab3c29a73c1e773149224de3
Daniel Black <daniel.black@carsforsale.com>
2016-09-27 07:35:15 -0500

    Using listview

--- components/App.js
+++ components/App.js
@@ -1,48 +1,94 @@
 import React, { Component } from 'react';
-import { ScrollView, Image, Text } from 'react-native';
+import { ListView, Image, Text, View } from 'react-native';
 import favicon from '../img/favicon.png';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        {
+          name: 'Daniel',
+          profileImg: favicon,
+        },
+        {
+          name: 'Nicholas',
+          profileImg: favicon,
+        },
+        {
+          name: 'Joshua',
+          profileImg: favicon,
+        },
+        {
+          name: 'Karen',
+          profileImg: favicon,
+        },
+        {
+          name: 'Adaline',
+          profileImg: favicon,
+        },
+        {
+          name: 'Paul',
+          profileImg: favicon,
+        },
+        {
+          name: 'Peter',
+          profileImg: favicon,
+        },
+        {
+          name: 'Amos',
+          profileImg: favicon,
+        },
+        {
+          name: 'Abraham',
+          profileImg: favicon,
+        },
+        {
+          name: 'Isaac',
+          profileImg: favicon,
+        },
+        {
+          name: 'Jacob',
+          profileImg: favicon,
+        },
+        {
+          name: 'Samuel',
+          profileImg: favicon,
+        },
+        {
+          name: 'Elijah',
+          profileImg: favicon,
+        },
+        {
+          name: 'Nathaniel',
+          profileImg: favicon,
+        },
+      ]),
+    };
   }
 
   render() {
     return (
-      <ScrollView>
-        <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Text style={{ fontSize: 96 }}>If you like</Text>
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Text style={{ fontSize: 96 }}>Scrolling down</Text>
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Text style={{ fontSize: 96 }}>What's the best</Text>
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Text style={{ fontSize: 96 }}>Framework around?</Text>
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Image source={favicon} />
-        <Text style={{ fontSize: 80 }}>React Native</Text>
-      </ScrollView>
+      <View style={{ paddingTop: 22 }}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={rowData =>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                alignItems: 'center' }}
+            >
+              <Text style={{ flex: 1 }}>{rowData.name}</Text>
+              <Image source={rowData.profileImg} />
+            </View>
+          }
+        />
+      </View>
     );
   }
 }
